ba04c09b32837e5f0d6e539a3117c1fdfca3586c
dari6475 <daniel.richardson@colorado.edu>
2026-09-04 15:57:13 -0600

    Update square_coax_example.py

--- statics_solver/square_coax_example.py
+++ statics_solver/square_coax_example.py
@@ -44,7 +44,7 @@ for i in range(cx - inner_hw, cx + inner_hw + 1):
 # Filling the entire "gap" area with a specific relative permittivity
 # We can just fill the whole area; the boundary nodes will override 
 # the fields inside the conductors anyway.
-sim.set_material([0, nx], [0, ny], value=3.0) # e.g., Random dielectric value I assigned it
+sim.set_material([0, nx], [0, ny], value=3.0) # e.g., Dielectric value I assigned it
 
 # --- Solve ---
 V = sim.solve()
