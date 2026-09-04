0083e48bcb0dc234efe59b2a1af4a06538919d30
dari6475 <daniel.richardson@colorado.edu>
2026-09-04 15:54:33 -0600

    Update run.sh

--- slurm_scripts/run.sh
+++ slurm_scripts/run.sh
@@ -33,6 +33,7 @@ conda activate sandbox
 # export LD_LIBRARY_PATH=$INSTALL_DIR/ngspice-sf-mirror-ngspice-44.2/local/lib:$LD_LIBRARY_PATH
 
 # Run simulation
-python fdtd_igp.py
-python fdtd_igp_clear.py
+python master.py
+# python master_clear.py
+# python master_metal.py
 python post_processor.py
